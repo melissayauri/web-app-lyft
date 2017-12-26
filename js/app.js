3b2b4ae21a7f3bd4ef1ea91674a675d1e8acc31d
melissayauri <yaurimelissa@gmail.com>
2017-12-26 04:45:44 -0500

    incorporando vista sign up

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
   /*vista splash por 4 segundos*/
-  $('.splash').delay(1000).fadeOut("slow");
+var loadImg =  $('.splash').delay(1000).fadeOut("slow");
 
+/*
+$("index.html").ready(loadImg);*/
+/*
+var cantDigCelular = function(){
+var cuadradoNegro = $("#cuadrado-negro");
+var longitud = $("#input-celular").val().length;
+console.log("longitud ");
+if (longitud == 9) {
+  $("#obtener-codigo").attr("href", "comprobacion-codigo.html");
+} else {
+  cuadradoNegro.text("El celular debe tener 9 dígitos");
+  cuadradoNegro.removeClass("ocultar");
+  setTimeout(function(){ cuadradoNegro.addClass("ocultar"); }, 3000);
+  $("#obtener-codigo").removeAttr("href");
+}
+}
+*/
 
+	$("#input-celular").focus();
+
+	$("#obtener-codigo").on("click",function(){
+    var cuadradoNegro = $("#cuadrado-negro");
+    var longitud = $("#input-celular").val().length;
+    console.log("longitud ");
+    if (longitud == 9) {
+      $("#obtener-codigo").attr("href", "comprobacion-codigo.html");
+    } else {
+      cuadradoNegro.text("El celular debe tener 9 dígitos");
+      cuadradoNegro.removeClass("ocultar");
+      setTimeout(function(){ cuadradoNegro.addClass("ocultar"); }, 3000);
+      $("#obtener-codigo").removeAttr("href");
+    }
+
+  })
 });
+/*$(document).ready(cargar);*/
