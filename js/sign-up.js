edabccf06c23f20c025e60756a0299eb3b87e468
melissayauri <yaurimelissa@gmail.com>
2017-12-28 03:10:56 -0500

    incorporando selector para el codigo postal

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,14 +1,17 @@
-var numx = (Math.floor((Math.random() * 10) + 1)).toString();
-var numy = (Math.floor((Math.random() * 10) + 1)).toString();
-var numz = (Math.floor((Math.random() * 10) + 1)).toString();
-
+/*numero aleatorios*/
+var numx = Math.floor((Math.random() * 9) + 1);
+var numy = Math.floor((Math.random() * 9) + 1);
+var numz = Math.floor((Math.random() * 9) + 1);
+/* #flag-venezuela,#flag-estados-unidos'*/
 $(document).ready(function() {
   /* funcion para hacer el cambio de bandera*/
   $('#flag-mexico,#flag-venezuela,#flag-estados-unidos').click(function() {
-    var locationFlag = $(this).attr('src');
+    var location = $(this).attr('src');
     /* se le asigna la nueva ubicacion de las banderas*/
-    $('#flag').attr('src', locationFlag);
+    $('#flag').attr('src', location);
   });
+
+
   /* funcion para validar el numero de celular e inabilitar el boton next*/
   $('#number-phone').keyup(function() {
     if (($(this).val().length) === 9) {
@@ -21,7 +24,6 @@ $(document).ready(function() {
   });
   /* generando la alerta con un codigo*/
   $('#next').click(function() {
-
     alert('lab-' + numx + numy + numz);
     /* redireccionando hacia el arhico verify*/
     window.location.href = '../views/verify-phone.html';
